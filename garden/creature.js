a4d92c018433a8c09ea662c16442086676be1fc1
bodiesh <bodieshargel@gmail.com>
2026-05-05 22:56:43 -0700

    class 5/5

---
+++ garden/creature.js
@@ -0,0 +1,8 @@
+$("#crAdd").click(function(){
+    let crName=$("#crName").val();
+    console.log(crName);
+
+    if( crName.length > 1) {$("#creature-list").append("<div>"+crName+"</div>");}
+
+    $("#crName").val("");
+});
